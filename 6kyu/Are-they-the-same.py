d2ccc9f8b801981931a3ceb813e4e68ccd4a4b78
Derek <dedavis93@gmail.com>
2023-07-25 23:29:02 -0500

    add solution

--- 6kyu/Are-they-the-same.py
+++ 6kyu/Are-they-the-same.py
@@ -1,8 +1,9 @@
 
-
 def comp(array1, array2):
+    if type(array) != list or type(array2) != list:
+        return False
     result = []
     for integer in array1:
-        result.append(integer * interger)
+        result.append(integer * integer)
     return sorted(result)  == sorted(array2)
 
